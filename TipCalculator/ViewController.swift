038e041dd5902124d20bed8c383d5adadad41cca
Minas Karagiorgis <minaskaragiorgis@minass-macbook-pro.local>
2018-12-14 15:08:08 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Minas Karagiorgis on 14/12/2018.
+//  Copyright © 2018 Minas Karagiorgis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
